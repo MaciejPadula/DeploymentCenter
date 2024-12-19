4d9b7d1e0042614d18d0504cd7eb5ccf7d5b11a8
Maciej Padula <maciej.padula@wp.pl>
2024-12-19 19:19:37 +0100

    additional template

--- src/DeploymentCenter/DeploymentCenter.Templates/Core/Repositories/DefaultTemplateRepository.cs
+++ src/DeploymentCenter/DeploymentCenter.Templates/Core/Repositories/DefaultTemplateRepository.cs
@@ -8,44 +8,11 @@ internal class DefaultTemplateRepository : ITemplateRepository
 
     public DefaultTemplateRepository()
     {
-        var deploy = new Resource(
-            "BasicDeploy",
-            ResourceType.Deployment,
-            [
-                new Variable("Namespace", "{namespace}"),
-                new Variable("ApplicationName", "{appName}"),
-                new Variable("ContainerName", "sql-server"),
-                new Variable("Image", "mcr.microsoft.com/mssql/server:2022-latest"),
-                new Variable("Name", "{appName}-deploy"),
-                new Variable("Replicas", "1"),
-                new Variable("Port", "1433"),
-                new Variable("HostPort", "1433"),
-                new Variable("EnvVariables", "MSSQL_SA_PASSWORD:{saPassword},ACCEPT_EULA:Y")
-            ]);
-
-        var lb = new Resource(
-            "BasicLb",
-            ResourceType.LoadBalancer,
-            [
-                new Variable("Namespace", "{namespace}"),
-                new Variable("ApplicationName", "{appName}"),
-                new Variable("Name", "{appName}-lb"),
-                new Variable("Ports", "{port}:1433"),
-                new Variable("IpAddresses", "{ipAddress}")
-            ]);
-
-        var sqlTemplate = new ResourceTemplate(
-            Guid.NewGuid(),
-            "sql-server",
-            [
-                new("appName", "Application Name", "sql-server"),
-                new("port", "Port", "1433"),
-                new("ipAddress", "Ip Address", "172.30.255.251"),
-                new("namespace", "Namespace", "default"),
-                new("saPassword", "SA Password", "zaq1@WSX")
-            ],
-            [deploy, lb]);
-        _templates = [sqlTemplate];
+        _templates =
+        [
+            TemplateFactory.CreateSimpleWebAppTemplate(),
+            TemplateFactory.CreateSqlServerTemplate(),
+        ];
     }
 
     public Task<ResourceTemplate?> GetTemplate(string name)
