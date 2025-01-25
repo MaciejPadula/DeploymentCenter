77a66a317af08c325b9f938bbbfd857907843902
Maciej Padula <maciej.padula@wp.pl>
2025-01-25 15:06:51 +0100

    fix breadcrumbs

--- src/DeploymentCenter.Frontend/deploymentcenter.frontend.client/src/shared/helpers/Redirect.tsx
+++
@@ -1,7 +0,0 @@
-import { useAppRouting } from "../hooks/navigation";
-
-export function Redirect(props: { to: string }) {
-  const navigation = useAppRouting();
-  navigation.navigateToUrl(props.to);
-  return <></>;
-}
